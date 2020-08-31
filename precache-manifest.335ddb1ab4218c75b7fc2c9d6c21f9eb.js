ebd043dfbf8b069104090a734442595c3c56af82
JohannesHool <hooljohannes@gmail.com>
2020-08-31 16:53:43 +0200

    Updates

---
+++ precache-manifest.335ddb1ab4218c75b7fc2c9d6c21f9eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad898194016caed0b2a9b4e24bcd077a",
+    "url": "/binaerbilder/index.html"
+  },
+  {
+    "revision": "841c045d35a3816ca3a4",
+    "url": "/binaerbilder/static/css/main.709070ff.chunk.css"
+  },
+  {
+    "revision": "4352f925e83b3d9e6859",
+    "url": "/binaerbilder/static/js/2.4f610097.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/binaerbilder/static/js/2.4f610097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "841c045d35a3816ca3a4",
+    "url": "/binaerbilder/static/js/main.6c0c9cd2.chunk.js"
+  },
+  {
+    "revision": "97d4da1fd4b116956558",
+    "url": "/binaerbilder/static/js/runtime-main.0de21dd5.js"
+  }
+]);
